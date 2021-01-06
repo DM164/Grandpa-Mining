4f503d0375e5a08638bba86c3608272b2edf31dc
Denis Mignanelli <denistschurtschenthaler@gmail.com>
2021-01-06 15:38:16 +0100

    less glow effect on status leds

--- statuses.js
+++ statuses.js
@@ -16,7 +16,7 @@ function retrieveServiceData() {
         modules.forEach(element => {
             if (element.value == 'maintenance') {
                 document.getElementById(`${element.name}-module-status`).style.background = '#3079ff'
-                document.getElementById(`${element.name}-module-status`).style.boxShadow = '0px 0px 8px 1px #649bff'
+                document.getElementById(`${element.name}-module-status`).style.boxShadow = '0px 0px 8px 0px #649bff'
                 maintenance = true
                 return
             }
@@ -25,7 +25,7 @@ function retrieveServiceData() {
 
             if (seconds > 60 && seconds < 120) {
                 document.getElementById(`${element.name}-module-status`).style.background = '#fbd23e'
-                document.getElementById(`${element.name}-module-status`).style.boxShadow = '0px 0px 8px 1px #ffe10b'
+                document.getElementById(`${element.name}-module-status`).style.boxShadow = '0px 0px 8px 0px #ffe10b'
                 offlineModules--
 
                 // Overall status update
@@ -34,7 +34,7 @@ function retrieveServiceData() {
                 document.getElementById('alice-text-status').innerText = 'Long response times'
             } else if (seconds < 60) {
                 document.getElementById(`${element.name}-module-status`).style.background = '#3efb4d'
-                document.getElementById(`${element.name}-module-status`).style.boxShadow = '0px 0px 8px 1px #31ff0b'
+                document.getElementById(`${element.name}-module-status`).style.boxShadow = '0px 0px 8px 0px #31ff0b'
                 allModules++
                 offlineModules--
             }
@@ -43,15 +43,15 @@ function retrieveServiceData() {
         if (maintenance == true) {
             // Overall status update
             document.getElementById('alice-status').style.background = '#3079ff'
-            document.getElementById('alice-status').style.boxShadow = '0px 0px 8px 1px #649bff'
+            document.getElementById('alice-status').style.boxShadow = '0px 0px 8px 0px #649bff'
             document.getElementById('alice-text-status').innerText = 'Maintenance'
         } else if (allModules == 5 && offlineModules == 0) {
             document.getElementById('alice-text-status').innerText = 'All systems online'
             document.getElementById('alice-status').style.background = '#3efb4d'
-            document.getElementById('alice-status').style.boxShadow = '0px 0px 8px 1px #31ff0b'
+            document.getElementById('alice-status').style.boxShadow = '0px 0px 8px 0px #31ff0b'
         } else if (offlineModules >= 1 && offlineModules != 5) {
             document.getElementById('alice-status').style.background = '#fbd23e'
-            document.getElementById('alice-status').style.boxShadow = '0px 0px 8px 1px #ffe10b'
+            document.getElementById('alice-status').style.boxShadow = '0px 0px 8px 0px #ffe10b'
             document.getElementById('alice-text-status').innerText = 'Partially online'
         }
     })
@@ -69,7 +69,7 @@ function retrieveServiceData() {
         modules.forEach(element => {
             if (element.value == 'maintenance') {
                 document.getElementById(`${element.name}-status`).style.background = '#3079ff'
-                document.getElementById(`${element.name}-status`).style.boxShadow = '0px 0px 8px 1px #649bff'
+                document.getElementById(`${element.name}-status`).style.boxShadow = '0px 0px 8px 0px #649bff'
                 maintenance = true
                 return
             }
@@ -78,16 +78,16 @@ function retrieveServiceData() {
 
             if (seconds > 60 && seconds < 120) {
                 document.getElementById(`${element.name}-status`).style.background = '#fbd23e'
-                document.getElementById(`${element.name}-status`).style.boxShadow = '0px 0px 8px 1px #ffe10b'
+                document.getElementById(`${element.name}-status`).style.boxShadow = '0px 0px 8px 0px #ffe10b'
                 offlineModules--
 
                 // Overall status update
                 document.getElementById('bot-status').style.background = '#fbd23e'
-                document.getElementById('bot-status').style.boxShadow = '0px 0px 8px 1px #ffe10b'
+                document.getElementById('bot-status').style.boxShadow = '0px 0px 8px 0px #ffe10b'
                 document.getElementById('bot-text-status').innerText = 'Long response times'
             } else if (seconds < 60) {
                 document.getElementById(`${element.name}-status`).style.background = '#3efb4d'
-                document.getElementById(`${element.name}-status`).style.boxShadow = '0px 0px 8px 1px #31ff0b'
+                document.getElementById(`${element.name}-status`).style.boxShadow = '0px 0px 8px 0px #31ff0b'
                 allModules++
                 offlineModules--
             }
@@ -96,15 +96,15 @@ function retrieveServiceData() {
         if (maintenance == true) {
             // Overall status update
             document.getElementById('bot-status').style.background = '#3079ff'
-            document.getElementById('bot-status').style.boxShadow = '0px 0px 8px 1px #649bff'
+            document.getElementById('bot-status').style.boxShadow = '0px 0px 8px 0px #649bff'
             document.getElementById('bot-text-status').innerText = 'Maintenance'
         } else if (allModules == 3 && offlineModules == 0) {
             document.getElementById('bot-text-status').innerText = 'All systems online'
             document.getElementById('bot-status').style.background = '#3efb4d'
-            document.getElementById('bot-status').style.boxShadow = '0px 0px 8px 1px #31ff0b'
+            document.getElementById('bot-status').style.boxShadow = '0px 0px 8px 0px #31ff0b'
         } else if (offlineModules >= 1 && offlineModules != 3) {
             document.getElementById('bot-status').style.background = '#fbd23e'
-            document.getElementById('bot-status').style.boxShadow = '0px 0px 8px 1px #ffe10b'
+            document.getElementById('bot-status').style.boxShadow = '0px 0px 8px 0px #ffe10b'
             document.getElementById('bot-text-status').innerText = 'Partially online'
         }
     })
@@ -121,31 +121,31 @@ function retrieveServiceData() {
 
             if (status == 'offline') {
                 document.getElementById(`${element.name}-status`).style.background = '#fb3e3e'
-                document.getElementById(`${element.name}-status`).style.boxShadow = '0px 0px 8px 1px #ff0b0b'
+                document.getElementById(`${element.name}-status`).style.boxShadow = '0px 0px 8px 0px #ff0b0b'
 
                 // Overall status update
                 document.getElementById('website-status').style.background = '#fbd23e'
-                document.getElementById('website-status').style.boxShadow = '0px 0px 8px 1px #ffe10b'
+                document.getElementById('website-status').style.boxShadow = '0px 0px 8px 0px #ffe10b'
                 document.getElementById('website-text-status').innerText = 'Partially online'
             } else if (status == 'maintenance') {
                 document.getElementById(`${element.name}-status`).style.background = '#3079ff'
-                document.getElementById(`${element.name}-status`).style.boxShadow = '0px 0px 8px 1px #649bff'
+                document.getElementById(`${element.name}-status`).style.boxShadow = '0px 0px 8px 0px #649bff'
                 maintenance = true
             } else {
                 document.getElementById(`${element.name}-status`).style.background = '#3efb4d'
-                document.getElementById(`${element.name}-status`).style.boxShadow = '0px 0px 8px 1px #31ff0b'
+                document.getElementById(`${element.name}-status`).style.boxShadow = '0px 0px 8px 0px #31ff0b'
 
                 // Overall status update
                 document.getElementById('website-text-status').innerText = 'All systems online'
                 document.getElementById('website-status').style.background = '#3efb4d'
-                document.getElementById('website-status').style.boxShadow = '0px 0px 8px 1px #31ff0b'
+                document.getElementById('website-status').style.boxShadow = '0px 0px 8px 0px #31ff0b'
             }
         });
         // Overall status update
         if (maintenance == true) {
             // Overall status update
             document.getElementById('website-status').style.background = '#3079ff'
-            document.getElementById('website-status').style.boxShadow = '0px 0px 8px 1px #649bff'
+            document.getElementById('website-status').style.boxShadow = '0px 0px 8px 0px #649bff'
             document.getElementById('website-text-status').innerText = 'Maintenance'
         }
     })
